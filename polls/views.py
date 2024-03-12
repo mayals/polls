f0017a55eaa1399ccba4f100a55b7ee08b85c8d9
May Al Sanea <developer.web.dj@gmail.com>
2024-03-12 15:46:10 +0400

    modify poll_delete_confirm

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from .models import Poll, Choice
 from .forms import PollForm,ChoiceForm
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -17,7 +17,7 @@ def home(request):
 
 
 
-
+@login_required(login_url='users:user-login')
 def poll_create(request):
     form = PollForm()
     if request.method =='POST':
@@ -42,7 +42,7 @@ def poll_create(request):
 
 
 
-
+@login_required(login_url='users:user-login')
 def poll_choices_create(request,poll_slug,year,month,day):
     poll = get_object_or_404(Poll, poll_slug=poll_slug,
                           published_at__year=year,
@@ -74,17 +74,64 @@ def poll_choices_create(request,poll_slug,year,month,day):
 
 
 
-def poll_detail(request,poll_slug,year,month,day):
-    poll = get_object_or_404(Poll, poll_slug=poll_slug,
-                          published_at__year=year,
-                         published_at__month=month,
-                           published_at__day=day
-    )
-    context = {
-        'poll': poll
-    }
-    return render(request, 'polls/poll_detail.html', context)
 
+
+
+
+
+@login_required(login_url='users:user-login')
+def poll_delete_confirm(request,year,month,day,poll_slug):
+    poll = get_object_or_404(Poll, status=Poll.Status.PUBLISHED,
+                                    poll_slug=poll_slug, 
+                                    published_at__year=year,
+                                    published_at__month=month,
+                                    published_at__day=day,
+    ) 
+    if poll.poll_user == request.user :
+        if request.method == 'POST' and 'yes-delete'in request.POST:
+            poll.delete()
+            messages.success(request,f'Thanks ( {request.user.username} ), your poll deleted successfully !')
+            return redirect('polls:home')
+                
+        context ={
+            'title': 'Poll Delete Confirm',
+            'poll' :  poll ,
+        }
+        return render(request,'polls/poll_delete_confirm.html',context)
+ 
+    else:
+        messages.warning(request,f"Sorry, you have no permission to delete this poll, only poll's author can delete it")
+        return redirect('polls:home')
+    
+
+
+
+
+
+@login_required(login_url='users:user-login')
+def poll_vote_create(request,poll_slug,year,month,day):
+    poll = get_object_or_404(Poll, poll_slug=poll_slug,
+                                published_at__year=year,
+                                published_at__month=month,
+                                published_at__day=day
+    )
+    
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+        selected_choice.choice_votes_count += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:poll-votes-result', kwargs={"poll_slug": poll.poll_slug,
+                                                                                    "year": poll.published_at.year,
+                                                                                   "month": poll.published_at.month,
+                                                                                     "day": poll.published_at.day}))
+    
+    except (KeyError, Choice.DoesNotExist):
+        context={
+            'poll': poll,
+            'error_message': "You didn't select a choice.",   
+        }
+        return render(request, 'polls/poll_detail.html', context)
+    
 
 
 
@@ -110,34 +157,20 @@ def poll_votes_result(request,poll_slug,year,month,day):
         'data_count'  : poll_choices_votes_counts 
         
     }
-    return render(request, 'polls/poll_votes_result.html', context)
+    return render(request, 'polls/poll_votes_result.html', context)        
 
 
 
 
-
-def poll_vote_create(request,poll_slug,year,month,day):
+def poll_detail(request,poll_slug,year,month,day):
     poll = get_object_or_404(Poll, poll_slug=poll_slug,
-                                published_at__year=year,
-                                published_at__month=month,
-                                published_at__day=day
+                          published_at__year=year,
+                         published_at__month=month,
+                           published_at__day=day
     )
-    
-    try:
-        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
-        selected_choice.choice_votes_count += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:poll-votes-result', kwargs={"poll_slug": poll.poll_slug,
-                                                                                    "year": poll.published_at.year,
-                                                                                   "month": poll.published_at.month,
-                                                                                     "day": poll.published_at.day}))
-    
-    except (KeyError, Choice.DoesNotExist):
-        context={
-            'poll': poll,
-            'error_message': "You didn't select a choice.",   
-        }
-        return render(request, 'polls/poll_detail.html', context)
-    
+    context = {
+        'poll': poll
+    }
+    return render(request, 'polls/poll_detail.html', context)
 
-        
+
