9291dee366dbc67a8e66bc4739f7a5bcdec5dbd3
May Al Sanea <developer.web.dj@gmail.com>
2024-03-16 13:50:23 +0400

    update settings.py after update ALLOWED_HOSTS

--- polls_proj/settings.py
+++ polls_proj/settings.py
@@ -45,7 +45,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://polls-z0im.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['polls-z0im.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
