ac699693ee4ee7417511fab59174d94ace2c9643
May Al Sanea <developer.web.dj@gmail.com>
2024-03-16 15:06:54 +0400

    remove any SINGLE OR DOUBLE QUOTES from .eev file because it is not allowed

--- polls_proj/settings.py
+++ polls_proj/settings.py
@@ -143,6 +143,10 @@ DATABASES = {
    'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600, conn_health_checks=True)
 }
 
+# print(DATABASES)
+
+
+SITE_URL = 'https://polls-z0im.onrender.com/'
 
 
 # Password validation
