03aa63e57ce5c6d830323af05bafacd348df9548
May Al Sanea <developer.web.dj@gmail.com>
2024-03-16 18:09:52 +0400

    modify staticfiles in urls.py

--- polls_proj/urls.py
+++ polls_proj/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +15,15 @@ urlpatterns = [
     # pages
     path('pages/',include('pages.urls', namespace='pages'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = 'pages.views.handling_404'
+
+
+
+# if settings.DEBUG:
+#     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
