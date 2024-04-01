95992cdbb04c1bc030a13361e9a0f7fb75ab30a2
May Al Sanea <developer.web.dj@gmail.com>
2024-04-01 06:26:43 +0400

    update settings

--- polls_proj/settings.py
+++ polls_proj/settings.py
@@ -51,7 +51,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['polls-dev-zqte.2.us-1.fl0.io','localhost','127.0.0.1']
 
